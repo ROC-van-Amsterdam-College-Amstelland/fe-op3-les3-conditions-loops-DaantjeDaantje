64a1dc13de91baa00469f50f04c54250dafd7cef
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-11-28 16:09:28 +0100

    Opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,7 @@
-//javascript code
+function myfuction(){
+    var mySaldo=50; 
+
+    if(mySaldo<25){
+        document.getElementById("mySaldo").innerHTML="Sorry, je hebt te weinig saldo!";
+    }
+}
