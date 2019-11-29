2a7054a3286da9c5089fa086ef929d5e8ebacb76
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-11-29 10:54:54 +0100

    Opdracht 2

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,11 @@
-//javascript code
+function myfuction(){
+    var mySaldo=50; 
+
+    if(mySaldo>=25){
+        document.getElementById("mySaldo").innerHTML="Je hebt voldoende saldo om 25 euro op te nemen.";
+    }
+
+    else{
+        document.getElementById("mySaldo").innerHTML="Sorry, je hebt te weinig saldo!"; 
+    }
+}
