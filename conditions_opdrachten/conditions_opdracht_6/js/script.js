d6072b7a07530693cfeb2c85ede5f339624bdcee
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-09 13:12:40 +0100

    Opdracht 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,17 @@
-//javascript code
+function myCalculator(myOperator){
+    var myNumber1 = parseInt(document.getElementById("input1").value);
+    var myNumber2 = parseInt(document.getElementById("input2").value);
+
+    if (myOperator == "+") {
+    document.getElementById('output').innerHTML = myNumber1 + myNumber2; 
+    }
+    else if (myOperator == "-") {
+        document.getElementById('output').innerHTML = myNumber1 - myNumber2; 
+        }
+        else if (myOperator == "*") {
+            document.getElementById('output').innerHTML = myNumber1 * myNumber2; 
+            }
+            else if (myOperator == "/") {
+                document.getElementById('output').innerHTML = myNumber1 / myNumber2; 
+                }  
+}
