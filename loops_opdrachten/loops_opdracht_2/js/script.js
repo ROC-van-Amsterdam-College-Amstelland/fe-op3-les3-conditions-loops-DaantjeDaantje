d665926d202100223e7d0cd206f1b01f6031f67f
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-13 11:24:27 +0100

    Loops2

--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+function myFunction(){
+    
+    var i;
+
+    for (var i=20; i>=0; i--){
+        document.getElementById("output").innerHTML += i + "<br>";
+        console.log(i); 
+    }
+
+}
