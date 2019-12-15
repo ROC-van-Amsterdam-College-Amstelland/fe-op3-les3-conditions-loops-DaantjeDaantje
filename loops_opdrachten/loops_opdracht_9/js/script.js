3017c0391064ceb8461bd1acfde9befb5eeab5a4
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-16 00:11:28 +0100

    Loops9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,23 @@
-//javascript code
+function myFunction(){
+    
+    var i;
+    var j;
+
+    for (var i=1; i<=5; i++){
+        document.getElementById("output").innerHTML += i + "<br>";
+        console.log(i);
+
+        if(i == 5){
+            document.getElementById("output").innerHTML += "Het einde is bereikt!<br>";
+        }
+    }
+
+    for (var j=5; j>=1; j--){
+        document.getElementById("output").innerHTML += j + "<br>";
+        console.log(j);
+
+        if(j == 1){
+            document.getElementById("output").innerHTML += "Het begin is het begin<br>";
+        }
+    }    
+}
