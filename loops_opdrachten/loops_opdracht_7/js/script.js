74ba7e2f10b7c681e442d07d16091afc52dd09a1
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-15 23:42:32 +0100

    Loops7

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,19 @@
-//javascript code
+function myFunction(){
+
+    for (var i=1; i<=100; i++){
+
+        document.getElementById("output").innerHTML += "<br>" + i;
+        console.log(i);
+
+        if (i%3 == 0 && i%5 == 0){
+            document.getElementById("output").innerHTML += " VET COOL";    
+        }
+        else if (i%3 == 0){
+            document.getElementById("output").innerHTML += " VET";    
+        }
+        
+        else if(i%5 == 0){
+            document.getElementById("output").innerHTML += " COOL";
+        }
+    }
+}
