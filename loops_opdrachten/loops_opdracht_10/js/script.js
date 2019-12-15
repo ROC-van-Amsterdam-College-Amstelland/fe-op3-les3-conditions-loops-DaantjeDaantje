e43c3c4c130f62601faa1178d2999a2bc39a7822
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-16 00:53:35 +0100

    Loops10

--- loops_opdrachten/loops_opdracht_10/js/script.js
+++ loops_opdrachten/loops_opdracht_10/js/script.js
@@ -1,1 +1,5 @@
-//javascript code
+function myFunction(){
+    var epic = ["a","b","c","d","e"];
+    Math.random();
+    document.getElementById("output").innerHTML = epic[Math.floor(Math.random()*5)];
+}
