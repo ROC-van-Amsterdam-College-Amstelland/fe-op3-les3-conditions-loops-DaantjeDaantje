c43723c5379239427525f7130f300f7a83cfa3ee
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-15 23:51:34 +0100

    Loops8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,12 @@
-//javascript code
+function myFunction(){
+    
+    var i;
+
+    for (var i=0; i<=50; i++){
+        document.getElementById("output").innerHTML += i + "<br>";
+        if(i == 25){
+        alert("Het getal 25 is net geen bullseye!");
+        }
+    }
+
+}
