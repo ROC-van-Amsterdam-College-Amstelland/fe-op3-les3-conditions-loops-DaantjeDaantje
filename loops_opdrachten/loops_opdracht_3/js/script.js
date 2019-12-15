f5625493d1645eab5144122aec8019e11ebfff0a
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-15 22:53:54 +0100

    Loops3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,17 @@
-//javascript code
+function myFunction(){
+    
+    var i;
+
+    for (var i=1; i<=50; i++){
+        document.getElementById("output").innerHTML += i + "<br>";
+        console.log(i); 
+
+        if (i%20 == 0){
+            document.getElementById("output").innerHTML += "Dit is een mooie getal<br>";    
+        }
+
+        else{
+            document.getElementById("output").innerhtml += i + "<br>";
+        }
+    }
+}
