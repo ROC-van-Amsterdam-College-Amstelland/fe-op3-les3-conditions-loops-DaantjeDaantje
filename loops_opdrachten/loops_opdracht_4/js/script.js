c0b25c02dd7337044b3d65941a821105ebb58183
DaantjeDaantje <danielleroelofsma@gmail.com>
2019-12-15 23:00:55 +0100

    Loops4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,9 @@
-//javascript code
+function myFunction(){
+    
+    var i;
+
+    for (var i=0; i<=1000; i+=10){
+        document.getElementById("output").innerHTML += i + "<br>";
+        console.log(i); 
+    }
+}
